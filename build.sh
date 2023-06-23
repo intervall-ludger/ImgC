d8caeea7d46e5d39f064d02a18127ce1b28204b6
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-06-23 15:33:25 +0200

    ImageConverter version updated: 1.0 -> 1.01

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 pyinstaller --onedir --windowed --icon=./icon.ico --hidden-import=imageio --collect-submodules=pydicom --add-data="./icon.ico;." ImageConverter.py --noconfirm
+pyinstaller --onefile --windowed --icon=./icon.ico --hidden-import=imageio --collect-submodules=pydicom --add-data="./icon.ico;." ImageConverter.py --noconfirm
